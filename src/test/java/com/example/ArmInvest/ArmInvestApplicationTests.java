28c5d6f640071488385930dff6ffd54ea5e997af
Naghash <naghashbaghdasaryang@gmail.com>
2019-10-23 19:47:55 +0200

    startingArmInvest

---
+++ src/test/java/com/example/ArmInvest/ArmInvestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ArmInvest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArmInvestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
